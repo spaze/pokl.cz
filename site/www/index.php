9ebedbc0f8d7538ca65f022439bf9c97a516c81f
Michal Špaček <mail@michalspacek.cz>
2016-04-02 04:19:58 +0200

    Add vulnerable admin page

--- site/www/index.php
+++ site/www/index.php
@@ -1,38 +1,5 @@
 <?php
-$https = (!empty($_SERVER['HTTPS']) && strcasecmp($_SERVER['HTTPS'], 'off'));
-$headers = array(
-	'Server' => 'Clouditycloudycloud',
-	'X-Powered-By' => 'Tenderloin, not the SF one',
-	'X-Content-Type-Options' => 'nosniff',
-	'X-XSS-Protection' => '1; mode=block',
-	'X-Frame-Options' => 'deny',
-);
-if ($https) {
-	$headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload';
-}
-
-foreach ($headers as $header => $value) {
-	header("{$header}: {$value}");
-}
-
-// Redirect to secure
-if (preg_match('/^(?:([^.]+\.))?([^.]+\.[^.]+)\z/', $_SERVER['HTTP_HOST'], $m)) {
-	if ($m[1] !== '' || !$https) {
-		$uri = $_SERVER['REQUEST_URI'];
-		header("Location: https://{$m[2]}{$uri}", true, 301);
-		exit;
-	}
-}
-
-$dataDir = '../data/';
-$cookieFile = $dataDir . 'cookie.json';
-$commentsInitialFile = $dataDir . 'comments.initial.json';
-$commentsFile = $dataDir . 'comments.json';
-
-$cookie = json_decode(file_get_contents($cookieFile));
-$signedIn = (isset($_COOKIE[$cookie->name]) && $_COOKIE[$cookie->name] === $cookie->value);
-$commentsInitial = json_decode(file_get_contents($commentsInitialFile));
-$comments = (json_decode(file_get_contents($commentsFile)) ?: []);
+require_once './prologue.php';
 
 if (isset($_POST['ccnum'], $_POST['ccmonth'], $_POST['ccyear'], $_POST['cccode'])) {
 	header('Location: /?status=ok');
